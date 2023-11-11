7102038b5c44198d6e4403b20226c87ad968a498
Davemafy <imafidondavid1@gmail.com>
2023-11-11 13:04:53 +0100

    ADDED More images , modifies index.html, css files , created js file to optimize image load

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const footer = document.querySelector('footer')
-let app = document.getElementById('update_area')
-let imgWrapper = document.querySelectorAll('.wrapper')
-const old = []
-let toggle = true
+const body = document.body;
+const bodyClone = document.createDocumentFragment();
+
+const footer = document.body.querySelector('footer');
+let app = document.getElementById('update_area');
+let imgWrapper = document.body.querySelectorAll('.wrapper');
+let old = []
+let toggle = true;
+let loadstates = document.querySelectorAll('.loading')
 
 function updateDom() {
   old.length < 3 ? old.push(app.innerHTML) : old;
@@ -27,9 +31,8 @@ function updateDom() {
         `
   } else {
     toggle = true
-    app.innerHTML = old[0];
+    app.innerHTML = old[0].slice(0, old[0].length)
   }
-  console.log(old);
 }
 
 let count = ''
@@ -38,36 +41,100 @@ window.onclick = (e) => {
   if (!e.target.closest('.wrapper img')) return;
   count++
   console.log(count);
-  document.querySelectorAll('.expand').forEach(item => item.classList.remove('expand'))
+  bodyClone.querySelectorAll('.expand').forEach(item => item.classList.remove('expand'))
   e.target.parentNode.classList.add('expand')
 }
 
 footer.addEventListener('click', () => updateDom())
 
-for (var i = 0; i <= 50; i++) {
-  if (i === 10) {}
-  imgWrapper.forEach(wrapper => {
-    wrapper.innerHTML += `
-     <div class="wrap-img" style="opacity: .6;">
+let divWrap = '';
+
+for (var i = 0; i <= 100; i++) {
+  if (i === 0) {
+    divWrap = `
+     <div class="loading" style="opacity: .6;">
+      <img id="item" src="" alt="">
+      </div>
+    `
+  }
+  divWrap += `
+     <div class="loading" style="opacity: .6;">
+      <div id="item" src="" alt="">
      </div>
     `
-
-  })
 }
+
+let container = [...imgWrapper]
+
+const wrapdiv = document.createDocumentFragment()
+
+size = imgWrapper.length
+
+for (var i = 0; i < size; i++) {
+  container[i].innerHTML = divWrap
+}
+
+imgWrapper.innerHTML = container
+
+let images = []
+limit = random(10)
+
+container = [...imgWrapper]
+
 console.log('divs created ');
-setTimeout(() => {
-  for (var i = 0; i <= 50; i++) {
-    imgWrapper.forEach((wrapper, index) => {
-      if (i === 0) {
-        wrapper.innerHTML = ' '
-      }
-      wrapper.innerHTML += `
-       <div class="wrap-img" style="opacity: 1;">
-         <img src="images/wall${i<5?i+1:3}.jpeg" alt="">
-       </div>
-      `
+
+const imageArr = document.querySelectorAll('#item')
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    for (var i = 0; i < limit; i++) {
+      imgWrapper.forEach((wrapper, index) => {
+        if (i === 0) {
+          a = random(25)
+          images[index] = `
+             <div class="wrap-img" style="opacity: 1;">
+               <img class="" src="images/wall${a}.jpg" alt="">
+             </div>
+          `
+        }
+        else if (i < limit) {
+          images[index] += `
+             <div class="wrap-img" style="opacity: 1;">
+               <img class="" src="images/wall${random(25) === a ? i : random(25)}.jpg" alt="">
+             </div>
+          `
+        }
+      })
+    }
+    container.forEach((wrapper, index) => {
+      wrapper.innerHTML = images[index]
+      wrapper.querySelectorAll('img').forEach(img => img.classList.remove('loading'))
+      wrapper.querySelectorAll('img').forEach(img => img.classList.add('loaded'))
     })
-  }
+  })
 })
 
-console.log('divs with images look aded');
+
+
+const nums = []
+
+function random(num) {
+  index = Math.floor((Math.random() * num) + 1)
+  return index;
+}
+
+
+
+//console.log('divs with images look aded');
+
+/*
+let a = document.createElement('a');
+let container = document.querySelector('.container');
+let documentFragment = document.createDocumentFragment();
+for (let i = 0; i < 1000; i++) {
+    let cloneA = a.cloneNode(true);
+    cloneA.text = `Row N° ${i}`;
+    documentFragment.appendChild(cloneA);
+  }
+  container.appendChild(documentFragment)
+
+*/
