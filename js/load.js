5e8713f4ef791f97fa26a757355c95f39df8451d
Davemafy <imafidondavid1@gmail.com>
2023-11-13 10:02:19 +0100

    optimized the imge load , created a transition between a small portion of it

---
+++ js/load.js
@@ -0,0 +1,82 @@
+let imgWrapper = document.body.querySelectorAll('.wrapper');
+
+let limit = 12;
+let renderLimit = 25;
+const arr = []
+
+preload()
+window.addEventListener('load', (e) => {
+  e.preventDefault()
+  render()
+})
+
+function preload() {
+  imgWrapper.forEach(wrapper => wrapper.innerHTML = spinner(12))
+}
+
+function render() {
+  const images = [];
+  const walls = document.querySelectorAll('#wall')
+
+  walls.forEach((wall, index) => {
+    if (wall.complete) {
+      document.body.classList.replace('loading', 'loaded')
+    } else {
+      wall.addEventListener('load', () => {
+        document.body.classList.replace('loading', 'loaded')
+      })
+    }
+  })
+}
+
+// generateImages(limit, renderLimit, images)
+
+
+
+function generateImages(limit, imgLength, images) {
+  for (var i = 0; i < limit; i++) {
+    imgWrapper.forEach((wrapper, index) => {
+      if (i === 0) {
+        images[index] = createImg('first', imgLength)
+      }
+      else if (i < limit) {
+        images[index] += createImg('random', imgLength)
+      }
+    })
+  }
+}
+
+function createImg(pick, imgLength) {
+  const firstPick = random(imgLength)
+  if (pick = 'random') {
+    return `<div class="wrap-img">
+             <img id="wall" style="object-fit: cover" src="thumbnails/wall${random(imgLength) === firstPick ? random(firstPick) : random(imgLength)}-min.jpg" alt="">
+          </div>`
+  }
+  else if (pick = 'first') {
+    return `<div class="wrap-img">
+              <img id="wall" style="object-fit: cover" src="thumbnails/wall${firstPick}-min.jpg" alt="">
+            </div>`
+  }
+}
+
+
+function random(num) {
+  const index = Math.floor((Math.random() * num) + 1)
+  return index;
+}
+
+function spinner(amount) {
+  let divWrap = '';
+;  for (var i = 0; i < amount; i++) {
+    let id = random(random(29))
+    divWrap += `<div style="background: url('/preload/pre${id}.jpg'); background-position: center;background-repeat: no-repeat!important;background-size: cover;">
+                    <img id="wall" style="object-fit: cover;" src="/thumbnails/wall${id}-min.jpg" loading="lazy" alt="">
+                  </div>
+               `
+  }
+  return divWrap;
+}
+//<img class="spinner" style="z-index: 1" src="/images/spinner.gif" alt="">
+// divWrap += `<div style="background: url('/preload/pre${i+1}.jpg'); background-position: center;background-repeat: no-repeat!important;background-size: cover;">
+//<img id="wall-bg" style="z-index: 0; object-fit: cover" src="/preload/pre${random(20) + random(9)}.jpg" loading="load" alt="">
--- js/main.js
+++ js/main.js
@@ -1,131 +1,69 @@
+//import { load } from './js/load.js'
+ 
 const body = document.body;
 const bodyClone = document.createDocumentFragment();
 
 const footer = document.body.querySelector('footer');
 let app = document.getElementById('update_area');
-let imgWrapper = document.body.querySelectorAll('.wrapper');
-let old = []
+const scrollSec = document.querySelectorAll('.wrapper');
+
+let old = [];
 let toggle = true;
 let loadstates = document.querySelectorAll('.loading')
 
-function updateDom() {
-  old.length < 3 ? old.push(app.innerHTML) : old;
 
-  if (toggle)
-  {
-    toggle = false
+footer.addEventListener('click', () => { updateDom('Dom')})
+
+function updateDom(text) {
+  old.length < 3 
+    ? 
+      old.push(app.innerHTML)
+    : 
+      old;
+  
+  if (toggle) {
+    toggle = false;
     app.innerHTML = `          
-          <header 
-            style="
-              padding: 1rem 1.5rem;
-            ">
+          <header style="padding: 1rem 1.5rem;">
             <nav class="app-bar" >
-              <h1 class="app-name">
-                DOM Updated!
-              </h1>
-              <button class="logo">
-                <img src="" alt&&="app-logo">
-              </button>
+              <h1 class="app-name"> ${text} </h1>
+              <button class="logo"><img src="/icons/infinity.svg" alt="app-logo"> </button>
             </nav>
-          </header>
-        `
-  } else {
-    toggle = true
-    app.innerHTML = old[0].slice(0, old[0].length)
+          </header> `;
+  }
+  else{
+    toggle = true;
+    app.innerHTML = old[0].slice(0, old[0].length);
   }
 }
 
-let count = ''
-window.onclick = (e) => {
-  let div = e.target.closest('div')
-  if (!e.target.closest('.wrapper img')) return;
-  count++
-  console.log(count);
-  bodyClone.querySelectorAll('.expand').forEach(item => item.classList.remove('expand'))
-  e.target.parentNode.classList.add('expand')
-}
-
-footer.addEventListener('click', () => updateDom())
-
-let divWrap = '';
-
-for (var i = 0; i <= 100; i++) {
-  if (i === 0) {
-    divWrap = `
-     <div class="loading" style="opacity: .6;">
-      <img id="item" src="" alt="">
-      </div>
-    `
-  }
-  divWrap += `
-     <div class="loading" style="opacity: .6;">
-      <div id="item" src="" alt="">
-     </div>
-    `
-}
-
-let container = [...imgWrapper]
-
-const wrapdiv = document.createDocumentFragment()
-
-size = imgWrapper.length
-
-for (var i = 0; i < size; i++) {
-  container[i].innerHTML = divWrap
-}
-
-imgWrapper.innerHTML = container
-
-let images = []
-limit = random(10)
-
-container = [...imgWrapper]
-
-console.log('divs created ');
-
-const imageArr = document.querySelectorAll('#item')
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    for (var i = 0; i < limit; i++) {
-      imgWrapper.forEach((wrapper, index) => {
-        if (i === 0) {
-          a = random(25)
-          images[index] = `
-             <div class="wrap-img" style="opacity: 1;">
-               <img class="" src="images/wall${a}.jpg" alt="">
-             </div>
-          `
-        }
-        else if (i < limit) {
-          images[index] += `
-             <div class="wrap-img" style="opacity: 1;">
-               <img class="" src="images/wall${random(25) === a ? i : random(25)}.jpg" alt="">
-             </div>
-          `
-        }
-      })
-    }
-    container.forEach((wrapper, index) => {
-      wrapper.innerHTML = images[index]
-      wrapper.querySelectorAll('img').forEach(img => img.classList.remove('loading'))
-      wrapper.querySelectorAll('img').forEach(img => img.classList.add('loaded'))
-    })
-  })
+scrollSec.forEach((scroller) => {
+   scroller.addEventListener('', scroll)
 })
 
-
-
-const nums = []
-
-function random(num) {
-  index = Math.floor((Math.random() * num) + 1)
-  return index;
+function scroll(e){
+ const childs =  e.target.querySelectorAll('div')
+  childs.forEach((child) => {
+    //parallax 
+  })
+ const imgs =  e.target.querySelectorAll('img')
+  imgs.forEach((child) => {
+    //parallax 
+  })
+  
 }
 
+function expand(e) {
+  unexpand();
+  e.target.classList.add('expand');
+}
+
+function unexpand() {
+  document.querySelectorAll('.expand').forEach(item => item.classList.remove('expand'))
+}
 
 
 //console.log('divs with images look aded');
-
 /*
 let a = document.createElement('a');
 let container = document.querySelector('.container');
@@ -137,4 +75,27 @@ for (let i = 0; i < 1000; i++) {
   }
   container.appendChild(documentFragment)
 
+*/
+/*
+let container = imgWrapper;
+
+const wrapdiv = document.createDocumentFragment();
+
+size = imgWrapper.length;
+for (var i = 0; i < size; i++) {
+  container[i].innerHTML = divWrap;
+}
+
+imgWrapper.innerHTML = container;
+*/
+/*
+window.onclick = (e) => {
+  
+  if (!e.target.closest('.wrapper img')) return;
+  
+  count++
+  console.log(count);
+  console.log(document.querySelectorAll('.expand'));
+  e.target.parentNode.classList.add('expand')
+}
 */
