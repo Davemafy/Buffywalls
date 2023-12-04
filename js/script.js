d06ac4e3dc7527ed1e5e10fbcdf1e3df06a7cb63
Davemafy <imafidondavid1@gmail.com>
2023-12-04 11:25:20 +0100

    feat: created the dynamic modal to show a popup to the user when an image is clicked

--- js/script.js
+++ js/script.js
@@ -1,36 +1,81 @@
-const btn = document.getElementById('menu-btn')
-const navMenu = document.getElementById('menu')
-const navMenuBack = document.getElementById('back')
-const walls = document.querySelectorAll('.wall')
+const walls = document.querySelectorAll('#wall')
 
 //setTimeout(openMenu, 100)
 
-btn.addEventListener('click', openMenu)
-navMenuBack.addEventListener('click', closeMenu)
 
-function openMenu(e){
-	navMenu.classList.add('open')
+window.addEventListener('click' , function(e){
+	let target = e.target 
+	
+	if(target.matches('#menu-btn , #menu-btn *') ){
+		openMenu()
+	}
+	 else if(target.matches('#back, #back *')){
+		closeMenu()
+	}
+	else if(target.matches('.img-track img')){
+		if(target.classList.contains('loaded') && !menu.classList.contains('open')){
+		  showModal(target)
+		}
+	} else if(target.matches('#closeModal, #closeModal *')){
+		modal.classList.remove('show')
+	}
+})
+
+function showModal(target){
+	let imgSrc = target.src
+
+	makeModal(imgSrc)
+	
+	modal.classList.add('show')
+	
+}
+
+function makeModal(src){
+  modalImg.setAttribute('src', src)
+}
+
+
+function openMenu(){
+	if(!menu && scaler) return;
+	
+	menu.classList.add('open')
 	scaler.classList.add('transit')
 }
-function closeMenu(e){
-	navMenu.classList.remove('open')
+
+
+function closeMenu(){
+	if(!menu && scaler) return;
+	
+	menu.classList.remove('open')
 	scaler.classList.remove('transit')
 }
+
+
+function setIndex(e){
+  let	target = e.target 
+	if(target.classList.contains('show')) return;
+	
+	//console.log('closed', target.classList)
+}
+
+modal.addEventListener('transitionend', setIndex)
+
+let effects = ['opacity-10']
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-    	entry.target.style.scale = "7"
-      entry.target.classList.remove('hide')
-      
-    } else {
-      entry.target.classList.add('hide')
-    	entry.target.style.scale = "0"
-      
-    }
+    	entry.target.parentElement.style.transition = '600ms'
+      entry.target.parentElement.classList.remove(...effects)
+   } else {
+  		entry.target.parentElement.style.transition = '500ms'
+      entry.target.parentElement.classList.add(...effects)
+  }
   })
 })
 
+
 walls.forEach((el) => observer.observe(el));
+
 
 
