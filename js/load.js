a4f3461efdc494f06a91c17fee86aa113f7cfef3
Davemafy <imafidondavid1@gmail.com>
2023-12-05 12:17:01 +0100

    feat: added loader functionaloty to the app

--- js/load.js
+++ js/load.js
@@ -1,56 +1,55 @@
-let imgWrapper = document.body.querySelectorAll('.img-track');
+let imgWrapper = document.body.querySelectorAll(".img-track");
 
 let limit = 10;
 let renderLimit = 25;
-const arr = []
+const arr = [];
 
-preload(limit)
-window.addEventListener('load', (e) => {
-  e.preventDefault()
-  
-  render()
-})
+preload(limit);
+window.addEventListener("load", e => {
+    render();
+});
 
 function preload(limit) {
-  imgWrapper.forEach(wrapper => wrapper.innerHTML = spinner(limit))
+    imgWrapper.forEach(wrapper => (wrapper.innerHTML = spinner(limit)));
 }
 
 function render() {
-  const walls = document.querySelectorAll('#wall')
+    const walls = document.querySelectorAll("#wall");
 
-  walls.forEach((wall, index) => {
-    if (wall.complete) {
-    	
-    	wall.classList.replace('loading','loaded')
-    	
-    	if(index == walls.length - 1){
-    	  document.body.classList.replace('loading', 'loaded')
-    	}
-    } else {
-      wall.addEventListener('load', () => {
-    		wall.classList.replace('loading','loaded')
-    		if(index == walls.length - 1){
-        	document.body.classList.replace('loading', 'loaded')
-    		}
-      })
-    }
-  })
+    walls.forEach((wall, index) => {
+        if (navigator.onLine) {
+            if (wall.complete) {
+                wall.classList.replace("loading", "loaded");
+
+                if (index == walls.length - 1) {
+                    document.body.classList.replace("loading", "loaded");
+                }
+            } else {
+                wall.addEventListener("load", () => {
+                    wall.classList.replace("loading", "loaded");
+                    if (index == walls.length - 1) {
+                        document.body.classList.replace("loading", "loaded");
+                    }
+                });
+            }
+        }
+    });
 }
 
 function random(num) {
-  const index = Math.floor((Math.random() * num) + 1)
-  return index;
+    const index = Math.floor(Math.random() * num + 1);
+    return index;
 }
 
 function spinner(amount) {
-  let divWrap = '';
-  
-  for (var i = 0; i < amount; i++) {
-  	let id = random(29)
-		 divWrap += `<div class="bg-lighterDark rounded-3xl" style="background: url('/preload/pre${id}.jpg'); background-position: center;background-repeat: no-repeat!important;background-size: cover;">
-                  <img id="wall" class="loading" src="thumbnails/wall${id}-min.jpg" loading="lazy" alt="">
-                </div>`
-  }
-  
-  return divWrap;
+    let divWrap = "";
+
+    for (var i = 0; i < amount; i++) {
+        let id = random(29);
+        divWrap += `<div>
+                  <img id="wall" class="loading" src="https://source.unsplash.com/random/?${id}" loading="lazy" alt="">
+                </div>`;
+    }
+
+    return divWrap;
 }
